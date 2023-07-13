b81a3266218be3cdc8e7b5428ff3435c10acde4d
Mustapha Mardi <mmardi@e3r4p6.1337.ma>
2023-07-13 21:55:57 +0100

    get chat from back seccesfuly

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -1,21 +1,27 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { connect } from 'http2';
 import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class ChatService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async createChat(user: { sub: string; username: string }, id: string) {
     try {
       const chat = await this.prisma.chat.create({
         data: {
           users: {
-            connect: {
-              id: user.sub
-            },
-          }
+            connect: [
+              {
+                id: user.sub
+              }, {
+                id: id
+              }
+            ]
+          },
+
         },
       });
-      return this.getConversation(user.sub,chat.id);
+      return this.getConversation(user.sub, chat.id);
     } catch (error) {
       throw error;
     }
@@ -47,12 +53,19 @@ export class ChatService {
     }
   }
   async getConversation(userId: string, chatId: string) {
-    const conversation = await this.prisma.chat.findMany({
+    const conversation = await this.prisma.chat.findUnique({
+      where: {
+        id:chatId
+      },
       include: {
-        users: true
-      }
+        users: true,
+        messages: true
+      },
+      
     });
-
+    conversation.users.map(user => delete user.password)
+    const conv = conversation.users.filter(user => user.id === userId)
+    if (!conv) throw new UnauthorizedException();
     return conversation;
   }
 }
